4a01dc62ef0f086e237791c7b2f65e7f025bb583
FuuNick <93815279+FuuNick@users.noreply.github.com>
2024-07-03 05:31:09 +0700

    Third

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Server is Running.");
+// Endpoint untuk mengirimkan kalimat "Hello World"
+app.get('/hello', (req, res) => {
+    res.send({ message: 'Hello World' });
 });
+
 
 app.listen(5000, console.log("server started on PORT 5000"));
